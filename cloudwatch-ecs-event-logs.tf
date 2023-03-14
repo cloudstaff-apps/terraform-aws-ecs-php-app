a0e39fd24f5c0ebe69c5ca498fa3c2e16b55d205
Nitin Bhadauria <nitinb@cloudstaff.com>
2023-03-14 14:02:59 +0530

    removed the cloudwatch log_resource_policy creation for every service

--- cloudwatch-ecs-event-logs.tf
+++ cloudwatch-ecs-event-logs.tf
@@ -26,25 +26,3 @@ resource "aws_cloudwatch_event_target" "ecs_events" {
   rule = aws_cloudwatch_event_rule.ecs_events.name
   arn  = aws_cloudwatch_log_group.ecs_events.arn
 }
-
-data "aws_iam_policy_document" "ecs_events" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:PutLogEventsBatch",
-    ]
-
-    resources = ["${aws_cloudwatch_log_group.ecs_events.arn}:*"]
-
-    principals {
-      identifiers = ["events.amazonaws.com", "delivery.logs.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_resource_policy" "ecs_events" {
-  policy_document = data.aws_iam_policy_document.ecs_events.json
-  policy_name     = "capture-ecs-events-${var.cluster_name}-${var.name}"
-}
